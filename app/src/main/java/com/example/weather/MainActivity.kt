9056a770f0f20513d55dd1af25d73342edc923d7
Maksim Doronkin <maksim3602@gmail.com>
2023-11-13 14:11:12 +0300

    add fetch weather

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,7 +2,15 @@ package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weather.databinding.ActivityMainBinding
+import org.json.JSONObject
+
+
+const val API_KEY = "c5dd137094604bcbb2b93330231311"
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +20,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bGet.setOnClickListener {
+            getResult("Voronezh")
+        }
+    }
+
+    private fun getResult(name: String) {
+        val url = "https://api.weatherapi.com/v1/current.json" +
+          "?key=$API_KEY&q=$name&aqi=no"
+
+        val queue = Volley.newRequestQueue(this)
+
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            url,
+            { response ->
+                val obj = JSONObject(response)
+                val temp = obj.getJSONObject("current")
+                Log.d("myLog", "Response: ${temp.getString("temp_c")}") },
+            { Log.d("myLog", "volley Error: $it") }
+        )
+
+        queue.add(stringRequest)
     }
 }
