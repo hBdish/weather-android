e6a070361615af5241cb8715b4cd1a654c05f6f9
Maksim Doronkin <maksim3602@gmail.com>
2023-11-14 19:47:10 +0300

    add fetch weather

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weather.fragment.MainFragment
 
-const val API_KEY = "c5dd137094604bcbb2b93330231311"
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,23 +14,4 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.placeholder, MainFragment.newInstance())
             .commit()
     }
-
-//    private fun getResult(name: String) {
-//        val url = "https://api.weatherapi.com/v1/current.json" +
-//          "?key=$API_KEY&q=$name&aqi=no"
-//
-//        val queue = Volley.newRequestQueue(this)
-//
-//        val stringRequest = StringRequest(
-//            Request.Method.GET,
-//            url,
-//            { response ->
-//                val obj = JSONObject(response)
-//                val temp = obj.getJSONObject("current")
-//                Log.d("myLog", "Response: ${temp.getString("temp_c")}") },
-//            { Log.d("myLog", "volley Error: $it") }
-//        )
-//
-//        queue.add(stringRequest)
-//    }
 }
--- app/src/main/java/com/example/weather/fragment/MainFragment.kt
+++ app/src/main/java/com/example/weather/fragment/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.weather.fragment
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,64 +11,110 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
-import com.example.weather.R
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weather.adapters.VpAdapter
+import com.example.weather.adapters.WeatherModel
 import com.example.weather.databinding.FragmentMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import org.json.JSONObject
+import java.lang.Error
+
+const val API_KEY = "c5dd137094604bcbb2b93330231311"
 
 class MainFragment : Fragment() {
 
-    private val fList = listOf(
-        HoursFragment.newInstance(),
-        DaysFragment.newInstance(),
+  private val fList = listOf(
+    HoursFragment.newInstance(),
+    DaysFragment.newInstance(),
+  )
+  private val tList = listOf(
+    "Hours",
+    "Days",
+  )
+
+  private lateinit var binding: FragmentMainBinding
+  private lateinit var pLauncher: ActivityResultLauncher<String>
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    binding = FragmentMainBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    checkPermission()
+    init()
+    requestWeatherData("Voronezh")
+  }
+
+  private fun init() = with(binding) {
+    val adapter = VpAdapter(activity as FragmentActivity, fList)
+    vp.adapter = adapter
+    TabLayoutMediator(tabLayout, vp) { tab, position ->
+      tab.text = tList[position]
+    }.attach()
+  }
+
+  private fun permissionListener() {
+    pLauncher = registerForActivityResult(
+      ActivityResultContracts
+        .RequestPermission()
+    ) {
+      Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+    }
+  }
+
+  private fun checkPermission() {
+    if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+      permissionListener()
+      pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+  }
+
+  private fun requestWeatherData(city: String) {
+    val url = "https://api.weatherapi.com/v1/forecast.json" +
+      "?key=$API_KEY" +
+      "&q=$city" +
+      "&days=2" +
+      "&aqi=no" +
+      "&alerts=no"
+
+    val queue = Volley.newRequestQueue(context)
+    val stringRequest = StringRequest(
+      Request.Method.GET,
+      url,
+      { response ->
+        parseWeatherData(response)
+      },
+      { Log.d("myLog", "volley Error: $it") }
     )
-    private val tList = listOf(
-        "Hours",
-        "Days",
+
+    queue.add(stringRequest)
+  }
+
+  private fun parseWeatherData(result: String) {
+    val mainObject = JSONObject(result)
+
+    val item = WeatherModel(
+      mainObject.getJSONObject("location").getString("name"),
+      mainObject.getJSONObject("current").getString("last_updated"),
+      mainObject.getJSONObject("current").getJSONObject("condition").getString("text"),
+      mainObject.getJSONObject("current").getJSONObject("condition").getString("icon"),
+      mainObject.getJSONObject("current").getString("temp_c"),
+      "",
+      "",
+      ""
     )
 
-    private lateinit var binding: FragmentMainBinding
-    private lateinit var pLauncher: ActivityResultLauncher<String>
+  }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        checkPermission()
-        init()
-    }
-
-    private fun init() = with(binding){
-        val adapter = VpAdapter(activity as FragmentActivity, fList)
-        vp.adapter = adapter
-       TabLayoutMediator(tabLayout, vp) { tab, position ->
-           tab.text = tList[position]
-       }.attach()
-    }
-
-    private fun permissionListener() {
-        pLauncher = registerForActivityResult(
-            ActivityResultContracts
-                .RequestPermission()) {
-            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
-        }
-    }
-
-    private fun checkPermission() {
-        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            permissionListener()
-            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-        }
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance() = MainFragment()
-    }
+  companion object {
+    @JvmStatic
+    fun newInstance() = MainFragment()
+  }
 }
